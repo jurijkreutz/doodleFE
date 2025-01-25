6cbe02072192edce8fa461374c00c50ac8190416
Jurij Kreutz <jkreutz@anexia-it.com>
2025-01-25 16:19:35 +0100

    DOO-42: WebSocket Staging Testing

--- doodle/src/environments/environment.staging.ts
+++ doodle/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   REST_URL: 'https://stag25.sketchoff.net/api',
-  WEBSOCKET_URL: 'wss://stag25.sketchoff.net/ws'
+  WEBSOCKET_URL: 'https://stag25.sketchoff.net/ws'
 };
