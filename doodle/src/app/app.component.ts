f3c6763d635d479ca5697323a2f2a920acfd8c28
jurijkreutz <jurij.kreutz97@gmail.com>
2025-03-05 21:26:26 +0100

    DOO-44: Show info to mobile users (only playable on desktop)

--- doodle/src/app/app.component.ts
+++ doodle/src/app/app.component.ts
@@ -17,6 +17,8 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 export class AppComponent implements OnInit, OnDestroy {
   title = 'doodle';
   isStart: boolean = false;
+  isGame: boolean = false;
+  isDesktop: boolean = window.innerWidth >= 1024;
 
   constructor(private router: Router,
               private restService: RestService,
@@ -28,7 +30,12 @@ export class AppComponent implements OnInit, OnDestroy {
       // @ts-ignore
     ).subscribe((event: NavigationEnd) => {
       this.isStart = (event as NavigationEnd).url === '/menu' || (event as NavigationEnd).url === '/';
-      console.log('isStart', this.isStart);
+      this.isGame = (event as NavigationEnd).url === '/game';
+      if (this.isStart || this.isGame || !this.isDesktop) {
+        document.body.classList.add('no-scroll');
+      } else {
+        document.body.classList.remove('no-scroll');
+      }
     });
   }
 
